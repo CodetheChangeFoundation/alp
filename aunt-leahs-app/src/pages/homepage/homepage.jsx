3aa8e094d205dd5583147ba51f323d07abbd1f78
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-10-26 13:55:18 -0700

    alp-11 basic page skeleton

---
+++ aunt-leahs-app/src/pages/homepage/homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const HomePage = () => (
+    <div className="homepage">
+        // HEADER
+
+        // LIST
+
+        // BUTTON
+
+        <a href="#" className="admin-login">ADMIN LOGIN</a>
+    </div>
+);
+
+export default HomePage;
