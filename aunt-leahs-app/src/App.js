136847db92b939a49a8ca9dbf7a70bdbd0a60970
Leonlui <leonlui@live.ca>
2019-11-02 13:24:44 -0700

    Commented out the volunteer page in app.js

--- aunt-leahs-app/src/App.js
+++ aunt-leahs-app/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import VolunteerPage from './pages/volunteerPage/volunteerPage.js'
-
-import './App.css';
 import HomePage from './pages/homepage/homepage';
+import VolunteerPage from './pages/volunteerPage/volunteerPage.js'
 
 function App() {
   return (
     <div>
       <HomePage/>
+      {/* <VolunteerPage/> */}
     </div>
   );
 }
