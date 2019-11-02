542a76019b924580623fb049f89b34d537294f20
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-11-02 14:33:31 -0700

    ALP-22: Removed subfolders and fixed imports

--- aunt-leahs-app/src/App.js
+++ aunt-leahs-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import HomePage from './pages/homepage/homepage';
+import HomePage from './pages/homepage';
 
 function App() {
   return (
---
+++ aunt-leahs-app/src/pages/homepage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Head from '../components/header';
+import LocationSelection from '../components/locationTable';
+import CustomButton from '../components/customButton'
+import constants from '../constants';
+import '../styles.css';
+
+const HomePage = () => (
+    <div className="homepage">
+        <Head page={constants.HEADER.SUB_HEADER.location} />
+
+        <div className="homepage-list">
+            <LocationSelection onLocationSelect={function setLocation(location) { alert("Selected!"); }} />
+        </div>
+
+        <div className="homepage-button">
+            <CustomButton size='small' color='primary'>Next</CustomButton>
+        </div>
+
+        <div className="homepage-admin-login">
+            <a href='#'>Admin Login</a>
+        </div>
+
+    </div>
+);
+
+export default HomePage;
+
