bdf4ffddeb1d228e7f89b6100ae234fa0efeaba7
Leonlui <leonlui@live.ca>
2019-11-02 13:31:26 -0700

    Deleted some unecessary stuff in selectbox component

--- aunt-leahs-app/src/components/select-box/select-box.jsx
+++ aunt-leahs-app/src/components/select-box/select-box.jsx
@@ -10,7 +10,6 @@ class SelectBox extends React.Component{
         super(props);
         this.state = {
             items: this.props.items || [],
-            showItems: false,
             selectedItem : this.props.items && this.props.items[0],
         }
     }
@@ -34,36 +33,6 @@ class SelectBox extends React.Component{
                 </Select>
             </FormControl>
             </div>
-        
-        
-        // <div>
-        //         <div className = "select-box--box"
-        //             style = {{width: this.props.width || 180}}> 
-        //             <div className = "select-box--container"> 
-        //                 <div className="select-box--selected-item">
-        //                     {this.state.selectedItem.value}
-        //                 </div>
-        //                 <div 
-        //                     className = "select-boc--arrow"
-        //                     onClick={this.dropDown}>
-        //                     <span className={`${this.state.showItems ? 'select-box-arrow-up' : 'select-box--arrow-down'}`}/>
-        //                 </div>
-        //                 <div
-        //                     style = {{display: this.state.showItems ? 'block': 'none'}}
-        //                     className = "select-box--items">
-        //                     {
-        //                         this.state.items.map(item => 
-        //                             <div
-        //                                 key={item.id}
-        //                                 onClick = {() => this.selectItem(item)}
-        //                                 className= {this.state.selectedItem === item ? 'selected' : ''}>
-        //                             { item.value}
-        //                             </div>)
-        //                     }
-        //                 </div>
-        //         </div>
-        //     </div>
-        // </div>
     }
 }
 export default SelectBox
