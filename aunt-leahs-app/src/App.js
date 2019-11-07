bf5703d7222167ceb72bbb66f1606c66e5fb2fa0
Leonlui <leonlui@live.ca>
2019-11-07 01:55:50 -0800

    Fixed indentation and comments for pull request

--- aunt-leahs-app/src/App.js
+++ aunt-leahs-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 import VolunteerPage from './pages/volunteerPage/volunteerPage.js'
@@ -7,8 +6,8 @@ import VolunteerPage from './pages/volunteerPage/volunteerPage.js'
 function App() {
   return (
     <div>
-      <HomePage/>
-      {/* <VolunteerPage/> */}
+      {/* <HomePage /> */}
+      <VolunteerPage />
     </div>
   );
 }
