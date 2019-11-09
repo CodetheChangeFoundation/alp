87427814f3c54c2b54ae9efb83b79268a2a161e2
Kai Sawamoto <stayhumblekai@gmail.com>
2019-11-09 13:33:08 -0800

    init commit

--- aunt-leahs-app/src/pages/volunteerData.jsx
+++ aunt-leahs-app/src/pages/volunteerData.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import Head from '../components/header/header';
+import CustomButton from '../components/customButton/customButton';
+import '../styles.css';
+
+const exportData = (data) => {
+	alert('Exporting data...');
+};
+
+const clearData = (data) => {
+	alert('Clearing data...');
+};
 
 export default function VolunteerData() {
 	return (
 		<div className="volunteerData">
-			<Head />
+			<Head className="volunteerDataHeader" />
+			<div className="volunteerDataTable">
+				<CustomButton size={'small'} color={'primary'} onClick={exportData}>
+					Export Data
+				</CustomButton>
+				<CustomButton size={'small'} color={'secondary'} onClick={clearData}>
+					Clear Data
+				</CustomButton>
+			</div>
 		</div>
 	);
 }
