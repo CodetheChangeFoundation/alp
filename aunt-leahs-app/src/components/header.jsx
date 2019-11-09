f45591b833316920f5077219666b08df1502ce0e
Cody Gagnon <14811557+codygagnon@users.noreply.github.com>
2019-11-09 13:14:49 -0800

    Update aunt-leahs-app/src/components/header.jsx

--- aunt-leahs-app/src/components/header.jsx
+++ aunt-leahs-app/src/components/header.jsx
@@ -8,7 +8,7 @@ const Head = ({ page }) => {
     /*takes in 'page' as props. Refer to ./components/constants.jsx for props.page values */
     const hasSubtext = page !== null 
     
-    return(
+    return (
         <div className='headerBox'>
                 <img className="headerImage header"  alt="Aunt Leah's Logo" src={require('../AuntLeahsTrees.png')}/>
                 <div className="headerMain header">{headerText.headTwo}</div>
