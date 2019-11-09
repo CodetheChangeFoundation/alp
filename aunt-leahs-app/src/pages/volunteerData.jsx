febe47921217a16eb06f40ea25df6c7e4f62a2ad
Kai Sawamoto <stayhumblekai@gmail.com>
2019-11-09 12:29:02 -0800

    pasted header

---
+++ aunt-leahs-app/src/pages/volunteerData.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Head from '../components/header/header';
+
+export default function VolunteerData() {
+	return (
+		<div className="volunteerData">
+			<Head />
+		</div>
+	);
+}
