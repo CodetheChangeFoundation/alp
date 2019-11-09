bc82e659dadfd99b07d019ca000074a652fd69b3
Michael <michaelfromyeg@gmail.com>
2019-11-09 13:05:12 -0800

    Added basic template for new volunteer page
    Waiting on text field, select field to be merged

--- aunt-leahs-app/src/App.js
+++ aunt-leahs-app/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import NewVolunteer from './pages/new-volunteer/new-volunteer';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <NewVolunteer/>
     </div>
   );
 }
