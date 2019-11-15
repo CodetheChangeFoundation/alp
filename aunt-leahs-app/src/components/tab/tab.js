ae84c7e3f102a82bc9c1c4df6d6136b6834b822f
Leonlui <leonlui@live.ca>
2019-11-14 17:10:42 -0800

    resolved tabPanelclass, fixed indentation, and changed variable naming

--- aunt-leahs-app/src/components/tab/tab.js
+++ aunt-leahs-app/src/components/tab/tab.js
@@ -1,83 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import { AppBar, Tabs, Tab, Typography, Box } from '@material-ui/core';
 
 
 // USAGE:
 {/* <TabComponent
-        name = "Tab"
-        items={[
+        tabs={[
             { title: 'First Tab', content: <h1>hello</h1>},
             { title: 'Second Tab',content: <CustomButton size="small" color="primary">I am a button</CustomButton>},
             { title: 'Third Tab', content: 'Some other class'}
             ...
         ]} /> */}
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-    return (
-        <Typography
-            component="div"
-            role="tabpanel"
-            hidden={value !== index}
-            id={`full-width-tabpanel-${index}`}
-            aria-labelledby={`full-width-tab-${index}`}
-            {...other}
-        >
-            <Box p={3}>{children}</Box>
-        </Typography>
-    );
-}
-  
-TabPanel.propTypes = {
-      children: PropTypes.node,
-      index: PropTypes.any.isRequired,
-      value: PropTypes.any.isRequired,
-};
-
-  
 class TabComponent extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             value: 0,
-            items: this.props.items || [],
+            tabs: this.props.tabs || [],
         }
     }
+    topLeftPadding =2;
 
     handleChange = (event, newValue) => {
         this.setState({ value: newValue })
     };
 
-    render () {return <div>
-        <AppBar position="static" color="default">
-            <Tabs
-                value={this.state.value}
-                onChange={this.handleChange}
-                indicatorColor="primary"
-                textColor="primary"
-                variant="fullWidth"
-                aria-label="tab label"
-            >
-            {this.state.items.map(item =>
-                <Tab label={item.title} />
-            )}
-            </Tabs>
-        </AppBar>
-        {this.state.items.map((item, index) =>
-            <TabPanel
-                value={this.state.value}
-                index={index}
-            >
-                {item.content}
-            </TabPanel>
-        )}
-      </div>
+    render () {
+        return (
+            <div>
+                {/* Forming Tabs with Title */}
+                <AppBar position="static" color="default">
+                    <Tabs
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        variant="fullWidth"
+                        aria-label="tab label"
+                    >
+                    {this.state.tabs.map(tab =>
+                        <Tab label={tab.title} />
+                    )}
+                    </Tabs>
+                </AppBar>
+
+                {/* Forming the content within the tabs and linking them back to the tab by index */}
+                {this.state.tabs.map((tab, index) =>
+                        <Typography
+                            component="div"
+                            role="tabpanel"
+                            hidden={this.state.value !== index}
+                            id={`full-width-tabpanel-${index}`}
+                            aria-labelledby={`full-width-tab-${index}`}
+                        >
+                            <Box p={this.topLeftPadding}>{tab.content}</Box>
+                        </Typography>
+                )}
+            </div>
+        )
     }
-  }
+}
 
 export default TabComponent
