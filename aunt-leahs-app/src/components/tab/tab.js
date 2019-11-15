7d8f1dedbba34d3791600512ac34173217c59a01
Leonlui <leonlui@live.ca>
2019-11-15 01:22:05 -0800

    Update aunt-leahs-app/src/components/tab/tab.js
    Co-Authored-By: Cody Gagnon <14811557+codygagnon@users.noreply.github.com>

--- aunt-leahs-app/src/components/tab/tab.js
+++ aunt-leahs-app/src/components/tab/tab.js
@@ -12,7 +12,7 @@ import { AppBar, Tabs, Tab, Typography, Box } from '@material-ui/core';
             ...
         ]} /> */}
 
-class TabComponent extends React.Component{
+class TabComponent extends Component {
     constructor(props){
         super(props);
         this.state = {
