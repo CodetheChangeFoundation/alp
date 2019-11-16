6127b5fff213c727e4bbee61b5f7e89179b05ce7
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-11-16 12:57:25 -0800

    ALP-16: Preliminary Router

--- aunt-leahs-app/src/App.js
+++ aunt-leahs-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage';
@@ -8,7 +9,9 @@ function App() {
 
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} /> 
+      </Switch>
     </div>
   );
 }
