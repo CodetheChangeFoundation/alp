a6b5b8b35109f6cc4fd003b4245caaf147ad13bb
Justin Kwan <justinkkwan@alumni.ubc.ca>
2019-11-17 01:56:58 -0800

    Converting old location list to new design

--- aunt-leahs-app/src/App.js
+++ aunt-leahs-app/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage';
-<<<<<<< HEAD
 import AdminLocationsPage from './pages/admin-locations';
 import './styles.css'
 
@@ -13,17 +12,6 @@ function App() {
 			<AdminLocationsPage />
 		</div>
 	);
-=======
-import './styles.css'
-
-function App() {
-
-  return (
-    <div className='testtest'>
-        <HomePage />
-    </div>
-  );
->>>>>>> 5f777980f023d20ac8f5bfc5873045f6df688942
 }
 
 export default App;
