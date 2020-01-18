14038cf0a194fbe6fb502649bc7504df1193cefd
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2020-01-18 13:48:16 -0800

    ALP-16: Router linking existing pages

--- aunt-leahs-app/src/App.js
+++ aunt-leahs-app/src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
+
+import HomePage from './pages/homepage';
 import AdminLogin from './pages/adminLogin';
+import VolunteerPage from './pages/volunteerPage';
 
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={HomePage} /> 
-        {/* Uncomment when other pages are merged
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/adminlogin' component={AdminLogin} />
         <Route exact path='/volunteerlogin' component={VolunteerPage}/>
-        <Route exact path='/adminlogin' component={AdminLogin}/>
-        */}
       </Switch>
     </div>
   );
