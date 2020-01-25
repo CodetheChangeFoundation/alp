2630784436319b7e31f69de516aa4fae5a98b13b
Cody Gagnon <14811557+codygagnon@users.noreply.github.com>
2020-01-25 12:06:58 -0800

    Update aunt-leahs-app/src/components/header/SubText.jsx
    fixed spacing

--- aunt-leahs-app/src/components/header/SubText.jsx
+++ aunt-leahs-app/src/components/header/SubText.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../styles.css'
 
-const SubText =(props)=>{
+const SubText = (props) => {
     return(
         <div className="header subtext">{props.title}</div>
     )
