76885f95faca1dca9115565115c02a7b679a9c29
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2020-02-01 13:15:27 -0800

    ALP-25 Rename of ShiftDataPage

--- aunt-leahs-app/src/pages/AdminShiftDataPage.jsx
+++ aunt-leahs-app/src/pages/AdminShiftDataPage.jsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AdminHeader from '../components/AdminHeader';
+import TableComponent from '../components/TableComponent';
+import CustomButton from '../components/customButton';
 
-const AdminShiftDataPage = () => {
-    return (
-        <div>
-            <AdminHeader />
-        </div>
-    )
+import { constants } from '../constants';
+
+export default class AdminShiftDataPage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			dataLastModified: {
+				// Tentative default. Not sure of database schema yet.
+				clear: '11/13/2019',
+				export: null
+			}
+		};
+		this.shiftData = constants.shiftData;
+	}
+
+	exportData = (data) => {
+		alert('Exporting data...');
+	};
+
+	clearData = (data) => {
+		alert('Clearing data...');
+	};
+
+	render() {
+		return (
+			<div>
+				<AdminHeader />
+				<div>
+					<div className="volunteerDataTableBody">
+						<TableComponent data={this.shiftData} />
+					</div>
+					<div className='volunteerDataBottom'>
+						<div className="lastModified">
+							<p>Last cleared: {this.state.dataLastModified.clear || 'Never'}</p>
+							<p>Last exported: {this.state.dataLastModified.export || 'Never'}</p>
+						</div>
+						<div className="volunteerDataButtons">
+							<div className="exportBtn">
+								<CustomButton size={'small'} color={'primary'} onClick={this.exportData}>
+									Export Data
+							</CustomButton>
+							</div>
+							<div className="clearBtn">
+								<CustomButton size={'small'} color={'secondary'} onClick={this.clearData}>
+									Clear Data
+							</CustomButton>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
-
-export default AdminShiftDataPage;
