c4715f8516ef2c0ed72c5312fd16ba870d7731e1
Leonlui <leonlui@live.ca>
2020-02-15 13:15:29 -0800

    WIP

--- aunt-leahs-app/src/pages/VolunteerCheckIn.jsx
+++ aunt-leahs-app/src/pages/VolunteerCheckIn.jsx
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 
-import { pages } from '../constants';
+import TextField from '@material-ui/core/TextField';
 
 import SelectBox from '../components/selectBox'
 import CustomButton from '../components/customButton'
@@ -14,51 +14,17 @@ import { setCurrentPage } from '../redux/page/pageAction';
 
 
 function VolunteerLoginPage({ history, setExistingVolunteer, setCurrentPage }) {
-	const volunteers = [
-		{ value: { firstName: 'LOOOL', lastName: 'Kumar' }, id: 1 },
-		{ value: { firstName: 'LOOOOL', lastName: 'Chauhan' }, id: 2 },
-		{ value: { firstName: 'LOOOOOOL', lastName: 'Doe' }, id: 3 },
-		{ value: { firstName: 'Justin', lastName: 'Kwan' }, id: 4 },
-		{ value: { firstName: 'Cody', lastName: 'Thechange' }, id: 5 }
-    ];
-    
-    const dates = [
-		{ value: { day: 'FEB 08', year: '2020' }, id: 1 },
-		{ value: { day: 'FEB 09', year: '2020' }, id: 2 },
-		{ value: { day: 'FEB 10', year: '2020' }, id: 3 }
-    ];
-    
-    const times = [
-		{ value: { time: '1:00'}, id: 1 },
-		{ value: { time: '2:00'}, id: 2 },
-		{ value: { time: '3:00'}, id: 3 }
+	const durations = [
+		{ value: { duration: '1 hour' }, id: 1 },
+		{ value: { duration: '2 hours' }, id: 2 },
+		{ value: { duration: '3 hours' }, id: 3 }
 	];
 
-    const durations = [
-		{ value: { duration: '1 hour'}, id: 1 },
-		{ value: { duration: '2 hours'}, id: 2 },
-		{ value: { duration: '3 hours'}, id: 3 }
-	];
-
-
-    let selectedVolunteer = null;
-    let selectedDate = null;
-    let selectedTime = null;
-    let selectedDurration = null;
-	function selectVolunteer(volunteer) {
-		selectedVolunteer = volunteer;
-    }
-
-    function selectDate(date){
-        selectedDate = date
-    }
-
-    function selectTime(time){
-        selectedTime = time
-    }
-    function selectDurration(durration){
-        selectedDurration = durration
-    }
+	let selectedVolunteer = null;
+	let selectedDurration = null;
+	function selectDurration(durration) {
+		selectedDurration = durration
+	}
 
 	const setVolunteerIfSelected = () => {
 		if (selectedVolunteer) {
@@ -71,37 +37,43 @@ function VolunteerLoginPage({ history, setExistingVolunteer, setCurrentPage }) {
 			alert('You have not selected any volunteers!');
 		}
 	}
-
+	const moment = require('moment');
+	let now = moment();
 
 	return (
 		<div className="App">
 			<Head page="Check In" />
-			<div style={{ margin: 'auto', width: '200px' }}>
-				<SelectBox
-					name="Date"
-					items={dates}
-					onSelectItem={selectDate}
+			<div>
+				<TextField
+					id="textField_date"
+					label="Date"
+					defaultValue={now.format("dddd, MMMM Do YYYY").toString()}
+					margin="normal"
+					InputProps={{
+						readOnly: true,
+					}}
+				/>
+			</div>
+			<div>
+				<TextField
+					id="textField_time"
+					label="Time"
+					defaultValue={now.format("LT").toString()}
+					margin="normal"
+					InputProps={{
+						readOnly: true,
+					}}
 				/>
 			</div>
 			<br />
-            <br />
-            <div style={{ margin: 'auto', width: '200px' }}>
-				<SelectBox
-					name="Time"
-					items={times}
-					onSelectItem={selectTime}
-				/>
-			</div>
-            <br />
-            <br />
-            <div style={{ margin: 'auto', width: '200px' }}>
+			<div style={{ margin: 'auto', width: '200px' }}>
 				<SelectBox
 					name="Duration"
 					items={durations}
 					onSelectItem={selectDurration}
 				/>
 			</div>
-            <br />
+			<br />
 			<div style={{ margin: '16px', position: 'relative' }}>
 				<CustomButton size="small" color="primary"
 					onClick={setVolunteerIfSelected}>
