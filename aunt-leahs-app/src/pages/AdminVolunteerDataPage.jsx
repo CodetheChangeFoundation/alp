0814dddfc2deb2f40e454ab552bad2f51aa2bd5f
Kai Sawamoto <kaisawamoto@gmail.com>
2020-03-20 22:28:35 -0700

    csv export ok

--- aunt-leahs-app/src/pages/AdminVolunteerDataPage.jsx
+++ aunt-leahs-app/src/pages/AdminVolunteerDataPage.jsx
@@ -6,18 +6,29 @@ import AdminHeader from '../components/AdminHeader';
 import CustomTable from '../components/CustomTable';
 import CustomButton from '../components/CustomButton';
 import { setCurrentPage } from '../redux/page/pageAction';
-
-import { constants } from '../constants';
+import { ExportToCsv } from 'export-to-csv';
 
 function AdminShiftDataPage({ setCurrentPage }) {
+	const [ dateLastModifiedClear, setDateLastModifiedClear ] = useState('');
+	const [ dateLastModifiedExport, setDateLastModifiedExport ] = useState('');
+	const [ volunteerData, setVolunteerData ] = useState([ '' ]); // useState(constants.volunteerData);
 
-	const [dateLastModifiedClear, setDateLastModifiedClear] = useState('');
-	const [dateLastModifiedExport, setDateLastModifiedExport] = useState('');
-	const [volunteerData, setVolunteerData] = useState(['']); // useState(constants.volunteerData);
-	//const volunteerData = constants.volunteerData;
-
-	const exportData = (data) => {
-		alert('Exporting data...');
+	const exportData = () => {
+		const options = {
+			fieldSeparator: ',',
+			filename: 'volunteerData',
+			quoteStrings: '"',
+			decimalSeparator: '.',
+			showLabels: true,
+			showTitle: true,
+			title: 'Volunteer Data',
+			useTextFile: false,
+			useBom: true,
+			useKeysAsHeaders: true
+			// headers: ['Column 1', 'Column 2', etc...] <-- Won't work with useKeysAsHeaders present!
+		};
+		const csvExporter = new ExportToCsv(options);
+		csvExporter.generateCsv(volunteerData);
 	};
 
 	const clearData = (data) => {
@@ -25,73 +36,66 @@ function AdminShiftDataPage({ setCurrentPage }) {
 		clearVolunteers();
 	};
 
-	
 	useEffect(() => {
 		getVolunteers();
 	}, []);
-	
 
 	async function getVolunteers() {
 		try {
 			const response = await fetch('http://localhost:7071/api/volunteers', {
 				method: 'GET',
 				//headers: {'Content-Type':'application/json'},
-				credentials: 'same-origin',  
+				credentials: 'same-origin'
 			});
 			const volunteers = await response.json();
 			setVolunteerData(volunteers);
+		} catch (error) {
+			console.log('Error fetching volunteers: ' + error);
 		}
-		catch (error) {
-			console.log("Error fetching volunteers: " + error);
-		}
-	};
+	}
 
 	async function clearVolunteers() {
 		try {
 			const response = await fetch('http://localhost:7071/api/DeleteVolunteersTrigger');
 			const reply = await response.json();
 			console.log(reply);
+		} catch (error) {
+			console.log('Error clearing volunteers: ' + error);
 		}
-		catch (error) {
-			console.log("Error clearing volunteers: " + error);
-		}
-	};
+	}
 
 	return (
+		<div>
+			<AdminHeader />
 			<div>
-				<AdminHeader />
-				<div>
-					<div className="volunteer-data-table-body">
-						<CustomTable data={ volunteerData } />
+				<div className="volunteer-data-table-body">
+					<CustomTable data={volunteerData} />
+				</div>
+				<div className="volunteer-data-bottom">
+					<div className="lastModified">
+						<p>Last cleared: {dateLastModifiedClear || 'Never'}</p>
+						<p>Last exported: {dateLastModifiedExport || 'Never'}</p>
 					</div>
-					<div className='volunteer-data-bottom'>
-						<div className="lastModified">
-							<p>Last cleared: { dateLastModifiedClear || 'Never'}</p>
-							<p>Last exported: { dateLastModifiedExport || 'Never'}</p>
+					<div className="volunteer-data-buttons">
+						<div className="export-btn">
+							<CustomButton size={'small'} color={'primary'} onClick={exportData}>
+								Export Data
+							</CustomButton>
 						</div>
-						<div className="volunteer-data-buttons">
-							<div className="export-btn">
-								<CustomButton size={'small'} color={'primary'} onClick={ exportData }>
-									Export Data
+						<div className="clearBtn">
+							<CustomButton size={'small'} color={'secondary'} onClick={clearData}>
+								Clear Data
 							</CustomButton>
-							</div>
-							<div className="clearBtn">
-								<CustomButton size={'small'} color={'secondary'} onClick={ clearData }>
-									Clear Data
-							</CustomButton>
-							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+		</div>
 	);
-};
+}
 
-const mapDispatchToProps = dispatch => ({
-	setCurrentPage: page => dispatch(setCurrentPage(page))
+const mapDispatchToProps = (dispatch) => ({
+	setCurrentPage: (page) => dispatch(setCurrentPage(page))
 });
 
-export default compose(
-	withRouter,
-	connect(null, mapDispatchToProps)
-)(AdminShiftDataPage);
+export default compose(withRouter, connect(null, mapDispatchToProps))(AdminShiftDataPage);
